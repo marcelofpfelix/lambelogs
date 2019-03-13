270d4f5d0e0c4392063fdd7efba793ca5e4997fc
Marcelo Félix <marcelo.felix@MacBook-Air.local>
2019-03-13 20:23:49 +0000

    ruby hash to json

--- readme.md
+++ readme.md
@@ -1,15 +1,14 @@
-# orchestratify
+# orchestify
 
 
 modules
 * https://www.npmjs.com/package/json-pretty-html
 * https://www.npmjs.com/package/express-prettify
 * https://www.npmjs.com/package/forms
-
+https://github.com/google/code-prettify
 
 
 references
 * https://www.codementor.io/codeforgeek/build-website-from-scratch-using-expressjs-and-bootstrap-du107sby7
 * https://gist.github.com/aerrity/fd393e5511106420fba0c9602cc05d35
 * https://ourcodeworld.com/articles/read/112/how-to-pretty-print-beautify-a-json-string
-* https://codepen.io/franciskim/pen/eNjrpR
--- views/client.js
+++ views/client.js
@@ -1,6 +1,45 @@
 
 console.log('Client-side code running');
 
+function rubyhash2json(str) {
+
+  //console.log(s);
+  console.log(str);
+
+    // replace symbol {:symbol=> to {"symbol"=>
+  str = str.replace(/(([{,]\s*)[:]([^>\s\[\]]+)|([{,]\s*)([0-9]+\.?[0-9]*))\s*=>/g, '$2$4"$3$5"=>');
+  console.log(`  1##########################################`);
+  console.log(str);
+  // replace {,'a' to {,"a"
+  str = str.replace(/([{,]\s*)'(.+?)'\s*=>/g, '$1"$2"=>');
+  console.log(`  7##########################################`);
+  console.log(str);
+  // replace =>'a' to :"a"
+  str = str.replace(/=>\s*'([^,}\s]+\s*)'/g, '=> "$1"');
+  console.log(`  8##########################################`);
+  console.log(str);
+    // replace value {"symbol"=>:value to {"symbol"=>"value"
+  str = str.replace(/([{,]\s*)(".+?"|[0-9]+\.?[0-9]*)\s*=>\s*:([^,}\s]+\s*)/g, '$1$2=>"$3"');
+  console.log(`  3##########################################`);
+  console.log(str);
+    // replace array [:a, :b] to ["a","b"]
+  str = str.replace(/([\[,]\s*):([^,\]\s]+)/g, '$1"$2"');
+  console.log(`  4##########################################`);
+  console.log(str);
+    // replace nil to null
+  str = str.replace(/=>nil/g, '=>null');
+  console.log(`  5##########################################`);
+  console.log(str);
+    // replace => to :
+  str = str.replace(/([{,]\s*)(".+?"|[0-9]+\.?[0-9]*)\s*=>/g, "$1$2:");
+  console.log(`  6##########################################`);
+  console.log(str);
+
+  return str;
+}
+
+
+
   // event listener to check when the DOM finished loading.
 document.addEventListener('DOMContentLoaded', function () {
 
@@ -9,23 +48,29 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function write_output() {
-      output_text.innerHTML = input_text.value;
 
 
-        var lines = $('#input').val().split(/\n/);
-        var output = [];
-        var outputText = [];
-        for (var i = 0; i < lines.length; i++) {
-          // only push this line if it contains a non whitespace character.
-          if (/\S/.test(lines[i])) {
-            outputText.push('">' + $.trim(lines[i]) + '<"');
-            output.push($.trim(lines[i]));
-          }
-        }
-        console.log(output);
-        output_text2.innerHTML = outputText;
-        $('#result2').val('[' + outputText + ']');
-        $('.alert').removeClass('alert-info').addClass('alert-success').text('Done!')
+    console.log(input_text.value);
+
+    s = rubyhash2json(input_text.value);
+
+    output_text.innerHTML = s;
+
+    var lines = $('#input').val().split(/\n/);
+    var output = [];
+    var outputText = [];
+    for (var i = 0; i < lines.length; i++) {
+      // only push this line if it contains a non whitespace character.
+      if (/\S/.test(lines[i])) {
+        outputText.push('">' + $.trim(lines[i]) + '<"');
+        output.push($.trim(lines[i]));
+      }
+    }
+    
+    console.log(output);
+    output_text2.innerHTML = outputText;
+    $('#result2').val('[' + outputText + ']');
+    $('.alert').removeClass('alert-info').addClass('alert-success').text('Done!')
 
 
   }
