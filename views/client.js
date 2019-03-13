c4fbfbdc0ca1ea6c0baafbef9c34f0d3a435ce40
marcelo felix <marcelofpfelix@gmail.com>
2019-03-13 01:05:45 +0000

    split new lines in response

--- views/client.js
+++ views/client.js
@@ -10,11 +10,31 @@ document.addEventListener('DOMContentLoaded', function () {
 
   function write_output() {
       output_text.innerHTML = input_text.value;
+
+
+        var lines = $('#input').val().split(/\n/);
+        var output = [];
+        var outputText = [];
+        for (var i = 0; i < lines.length; i++) {
+          // only push this line if it contains a non whitespace character.
+          if (/\S/.test(lines[i])) {
+            outputText.push('">' + $.trim(lines[i]) + '<"');
+            output.push($.trim(lines[i]));
+          }
+        }
+        console.log(output);
+        output_text2.innerHTML = outputText;
+        $('#result2').val('[' + outputText + ']');
+        $('.alert').removeClass('alert-info').addClass('alert-success').text('Done!')
+
+
   }
 
   var asd, input_text = document.querySelector("#input"),
-      output_text = document.querySelector("#result");
+     output_text = document.querySelector("#result");
+  output_text2 = document.querySelector("#result2");
   input_text.addEventListener("input", timer, !1), write_output(), input_text.select()
+
 
 
 
